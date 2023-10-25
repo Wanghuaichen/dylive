4a5f91622efdd4d9564260c747742c6ed424fbcb
cenglou <13975484008@163.com>
2023-10-25 10:50:16 +0800

    feat: add requirements.txt

--- csss.py
+++ csss.py
@@ -2,9 +2,3 @@
 # ctx=execjs.compile(open('cs.js', 'r', encoding='gb2312', errors='ignore').read())
 # string = ctx.call('my_function')
 # print(string)
-from faker import Faker
-# 中文需要使用zh_CN
-fake = Faker('zh_CN')
-# print('随机女性名称:')
-for i in range(10):
-    print(fake.paragraph())
